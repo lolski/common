160b5b8db3ce6c150ea0b7507ea067fc3c2da216
Joshua Send <joshuasend1@gmail.com>
2020-10-26 18:57:23 +0000

    clean up cooperative and non cooperative actor tests

---
+++ poc/CooperativeTest.java
@@ -0,0 +1,85 @@
+package grakn.common.poc;
+
+import grakn.common.concurrent.NamedThreadFactory;
+import grakn.common.concurrent.actor.Actor;
+import grakn.common.concurrent.actor.ActorRoot;
+import grakn.common.concurrent.actor.eventloop.EventLoopSingleThreaded;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class CooperativeTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopSingleThreaded eventLoop = new EventLoopSingleThreaded(NamedThreadFactory.create(CooperativeTest.class, "main"));
+        ArrayList<Long> output = new ArrayList<>();
+        Actor<ActorRoot> rootActor = Actor.root(eventLoop, ActorRoot::new);
+        Actor<CooperativeStarter> starter = rootActor.ask(root -> root.<CooperativeStarter>createActor((self) -> new CooperativeStarter(self, output))).await();
+        starter.tell((actor) -> actor.start());
+        // kill manually for now
+        Thread.sleep(1000);
+    }
+}
+
+class CooperativeStarter extends Actor.State<CooperativeStarter> {
+    private final Actor<CooperativeActor> cooperative;
+    private long startTime;
+    private ArrayList<Long> output;
+    private int completed = 0;
+    private int repeat = 1;
+
+    protected CooperativeStarter(final Actor<CooperativeStarter> self, ArrayList<Long> output) {
+        super(self);
+        this.output = output;
+        cooperative = this.<CooperativeActor>child((actor) -> new CooperativeActor(actor, output, self));
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        for (int i = 0; i < repeat; i++) {
+            cooperative.tell((actor) -> actor.run());
+        }
+    }
+
+    public void done() {
+        completed++;
+        if (completed == repeat) {
+            long elapsed = System.currentTimeMillis() - startTime;
+            System.out.println("Time taken for " + repeat + " cooperative repeats (ms): " + (elapsed));
+            System.out.println("Length of output array: " + (output.size()));
+        }
+    }
+}
+
+class CooperativeActor extends Actor.State<CooperativeActor> {
+
+    private final MockTransaction tx;
+    private final ArrayList<Long> output;
+    private final Actor<CooperativeStarter> parent;
+
+    protected CooperativeActor(final Actor<CooperativeActor> self, ArrayList<Long> output, Actor<CooperativeStarter> parent) {
+        super(self);
+        this.parent = parent;
+        this.output = output;
+        tx = new MockTransaction(10000000, 1000);
+    }
+
+    public void run() {
+        Iterator<Long> answers = tx.query();
+        if (answers.hasNext()) send(answers.next());
+        self().tell((actor) -> actor.resume(answers));
+    }
+
+    private void send(final Long l) {
+        output.add(l);
+    }
+
+    public void resume(Iterator<Long> r) {
+        if (r.hasNext()) send(r.next());
+        if (r.hasNext()) {
+            self().tell((actor) -> actor.resume(r));
+        } else {
+            parent.tell((actor) -> actor.done());
+        }
+    }
+}
---
+++ poc/MockTransaction.java
@@ -0,0 +1,43 @@
+package grakn.common.poc;
+
+import java.util.Iterator;
+
+/**
+ * Pretend to compute an iterator of longs, which just adds up numbers and returns
+ * them in intervals.
+ *
+ * Repeatedly calling query() will repeat the same calculation loop in a new iterator
+ */
+public class MockTransaction {
+    private long computeLength;
+    private int answerInterval;
+
+    public MockTransaction(long computeLength, int answerInterval) {
+        this.computeLength = computeLength;
+        this.answerInterval = answerInterval;
+    }
+
+    public Iterator<Long> query() {
+        return new Iterator<Long>() {
+            long count = 0L;
+
+            @Override
+            public boolean hasNext() {
+                return count < computeLength;
+            }
+
+            @Override
+            public Long next() {
+                while (count < computeLength) {
+                    if (count % answerInterval == 0) {
+                        count++;
+                        return count - 1;
+                    } else {
+                        count++;
+                    }
+                }
+                return null;
+            }
+        };
+    }
+}
