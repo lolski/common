3c18da292f8a64204787acf728619c4a80d80fa5
Joshua Send <joshuasend1@gmail.com>
2020-11-15 18:07:54 +0000

    Add Answer.java with explanation prototype

---
+++ poc/reasoning/Answer.java
@@ -0,0 +1,61 @@
+package grakn.common.poc.reasoning;
+
+import grakn.common.collection.Pair;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Answer {
+
+    // for retrieving explanations from the server
+    private int queryId;
+
+    Map<String, String> idMap;
+    private final String pattern;
+
+    public Answer(Map<String, String> idMap, String pattern, int queryId) {
+        this.idMap = idMap;
+        this.pattern = pattern;
+        this.queryId = queryId;
+    }
+
+    public Stream<Map<String, Explanation>> getExplanations() {
+        return tx.explanations(queryId, idMap);
+    }
+
+    public boolean isInferred() {
+        // conceptually, may not be final impl
+        return getExplanations() != null;
+    }
+}
+
+class Explanation {
+    String ruleName;
+    private Map<String, Answer> inferences;
+    // maps rule `when` conjunction sub-patterns to answers
+    private Explanation(final Map<String, Answer> inferences) {
+        this.inferences = inferences;
+    }
+}
+
+
+/*
+
+A1 Answer [friendship(x,z) and hasname(x,n), x = 1, z = 3, n = -1]
+      |
+{ friendship(x,z): Explanation[mutual-friendship-rule, friendship(x,y) -> A2, friendship(y, z) -> A3] }
+                               /                          \
+A2 Answer [friendship(x,y), x = 1, y = 2]             A3 Answer[friendship(y, z) -> y = 2, z = 3]
+
+
+
+A1 Answer [friendship(x,z) and hasname(x,n), x = 1, z = 3, n = -1]
+      |
+{ friendship(x,z): Explanation[mutual-friendship-rule, friendship(x,y) -> A2, friendship(y, z) -> A3] ,
+  hasname(x,n)   : Explanation[has-name,  ... -> A4]}
+                               /                          \
+A2 Answer [friendship(x,y), x = 1, y = 2]             A3 Answer[friendship(y, z) -> y = 2, z = 3]
+
+
+ */
+
