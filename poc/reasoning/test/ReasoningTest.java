73ce8e5b99ea22c9ca7d0c7ecc090d32e63c7ab4
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-11-16 10:33:44 +0000

    Fix singleAtomicActor caused by deduplication. Update variable names with the right terminologies

--- poc/reasoning/test/ReasoningTest.java
+++ poc/reasoning/test/ReasoningTest.java
@@ -24,21 +24,21 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
         registry.registerAtomic(0L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 5L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 5L, list())));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(0L), 5L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(0L), 5L, -10L, responses));
 
         long startTime = System.currentTimeMillis();
         long n = 5L + 5L + 1; //total number of traversal answers
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -59,24 +59,24 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
         registry.registerAtomic(2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 2L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 2L, list())));
 
         registry.registerAtomic(20L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 2L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 2L, list())));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(20L, 2L), 0L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(20L, 2L), 0L, 0L, responses));
 
         long startTime = System.currentTimeMillis();
         long n = 0L + (2L * 2L) + 1; //total number of traversal answers, plus one expected DONE (-1 answer)
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -97,24 +97,24 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
         registry.registerAtomic(2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 2L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 2L, list())));
 
         registry.registerAtomic(20L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 0L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 0L, list())));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(20L, 2L), 0L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(20L, 2L), 0L, 0L, responses));
 
         long startTime = System.currentTimeMillis();
         long n = 0L + (0L * 0L) + 1; // total number of traversal answers, plus one expected DONE (-1 answer)
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -135,26 +135,26 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
         registry.registerAtomic(-2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 1L, list())));
 
         registry.registerRule(list(-2L), pattern ->
-                        Actor.create(eventLoop, self -> new Rule(self, pattern, 1L, 0L)));
+                        Actor.create(elg, self -> new Rule(self, pattern, 1L, 0L)));
 
         registry.registerAtomic(2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, list(list(-2L)))));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 1L, list(list(-2L)))));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(2L), 0L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(2L), 0L, 0L, responses));
         long startTime = System.currentTimeMillis();
         long n = 0L + (1L) + (1L) + (1L) + 1; //total number of traversal answers, plus one expected DONE (-1 answer)
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -174,29 +174,29 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
-        Actor<Atomic> bottomAtomic = registry.registerAtomic(-2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, list())));
+        registry.registerAtomic(-2L, pattern ->
+                        Actor.create(elg, self -> new Atomic(self, pattern, 1L, list())));
 
         registry.registerRule(list(-2L), pattern ->
-                        Actor.create(eventLoop, self -> new Rule(self, pattern, 1L, 0L)));
+                        Actor.create(elg, self -> new Rule(self, pattern, 1L, 0L)));
 
-        Actor<Atomic> atomicWithRule = registry.registerAtomic(2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, list(list(-2L)))));
+        registry.registerAtomic(2L, pattern ->
+                        Actor.create(elg, self -> new Atomic(self, pattern, 1L, list(list(-2L)))));
 
-        Actor<Atomic> atomicWithoutRule = registry.registerAtomic(20L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, list())));
+        registry.registerAtomic(20L, pattern ->
+                        Actor.create(elg, self -> new Atomic(self, pattern, 1L, list())));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(20L, 2L), 0L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(20L, 2L), 0L, 0L, responses));
         long startTime = System.currentTimeMillis();
         long n = 0L + (1 * 1) + (1 * 2) + 1; //total number of traversal answers, plus one expected DONE (-1 answer)
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -217,26 +217,26 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
         registry.registerAtomic(2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 2L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 2L, list())));
 
         registry.registerAtomic(20L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 2L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 2L, list())));
 
         registry.registerAtomic(200L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 2L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 2L, list())));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(200L, 20L, 2L), 0L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(200L, 20L, 2L), 0L, 0L, responses));
         long startTime = System.currentTimeMillis();
         long n = 0L + (2L * 2L * 2L) + 1;
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -255,33 +255,33 @@ public class ReasoningTest {
         Registry registry = new Registry();
 
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // create atomic actors first to control answer size
         registry.registerAtomic(2L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 10L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 10L, list())));
 
         registry.registerAtomic(20L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 10L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 10L, list())));
 
         registry.registerAtomic(200L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 10L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 10L, list())));
 
         registry.registerAtomic(2000L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 10L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 10L, list())));
 
         registry.registerAtomic(20000L, pattern ->
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 10L, list())));
+                        Actor.create(elg, self -> new Atomic(self, pattern, 10L, list())));
 
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(20000L, 2000L, 200L, 20L, 2L), 0L, 0L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(20000L, 2000L, 200L, 20L, 2L), 0L, 0L, responses));
         long startTime = System.currentTimeMillis();
         long n = 0L + (10L * 10L * 10L * 10L * 10L) + 1;
         System.out.println("Number of requests: " + n);
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -300,7 +300,7 @@ public class ReasoningTest {
     public void bulkActorCreation() throws InterruptedException {
         Registry registry = new Registry();
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         List<List<Long>> rules = new ArrayList<>();
         for (long i = 2L; i < 1000_000L; i++) {
@@ -308,12 +308,12 @@ public class ReasoningTest {
         }
         long start = System.currentTimeMillis();
         registry.registerAtomic(1L, pattern ->
-                Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, rules))
+                Actor.create(elg, self -> new Atomic(self, pattern, 1L, rules))
         );
-        Actor<Conjunction> conjunctive = Actor.create(eventLoop, self -> new Conjunction(self, list(1L), 0L, 0L, responses));
-        conjunctive.tell(actor ->
+        Actor<Conjunction> conjunction = Actor.create(elg, self -> new Conjunction(self, list(1L), 0L, 0L, responses));
+        conjunction.tell(actor ->
                 actor.executeReceiveRequest(
-                        new Request(new Request.Path(conjunctive), list(), list(), list()),
+                        new Request(new Request.Path(conjunction), list(), list(), list()),
                         registry
                 )
         );
@@ -326,18 +326,18 @@ public class ReasoningTest {
     public void loopTermination() throws InterruptedException {
         Registry registry = new Registry();
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         // conjunction1 -> atomic1 -> rule1 -> atomic1
-        registry.registerRule(list(1L), pattern -> Actor.create(eventLoop, self -> new Rule(self, pattern, 1L, 0L)));
-        registry.registerAtomic(1L, pattern -> Actor.create(eventLoop, self -> new Atomic(self, pattern, 1L, list(list(1L)))));
-        Actor<Conjunction> conjunctive = Actor.create(eventLoop, self -> new Conjunction(self, list(1L), 0L, 0L, responses));
+        registry.registerRule(list(1L), pattern -> Actor.create(elg, self -> new Rule(self, pattern, 1L, 0L)));
+        registry.registerAtomic(1L, pattern -> Actor.create(elg, self -> new Atomic(self, pattern, 1L, list(list(1L)))));
+        Actor<Conjunction> conjunction = Actor.create(elg, self -> new Conjunction(self, list(1L), 0L, 0L, responses));
 
         long n = 0L + 1L + 1L + 1L + 1;
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
@@ -354,20 +354,20 @@ public class ReasoningTest {
     public void deduplication() throws InterruptedException {
         Registry registry = new Registry();
         LinkedBlockingQueue<List<Long>> responses = new LinkedBlockingQueue<>();
-        EventLoopGroup eventLoop = new EventLoopGroup(1, "reasoning-elg");
+        EventLoopGroup elg = new EventLoopGroup(1, "reasoning-elg");
 
         registry.registerAtomic(1L, pattern ->
 
-                        Actor.create(eventLoop, self -> new Atomic(self, pattern, 100L, list()))
+                        Actor.create(elg, self -> new Atomic(self, pattern, 100L, list()))
         );
-        Actor<Conjunction> conjunctive =
-                Actor.create(eventLoop, self -> new Conjunction(self, list(1L), 100L, 1L, responses));
+        Actor<Conjunction> conjunction =
+                Actor.create(elg, self -> new Conjunction(self, list(1L), 100L, 1L, responses));
 
         long n = 100L + 1L;
         for (int i = 0; i < n; i++) {
-            conjunctive.tell(actor ->
+            conjunction.tell(actor ->
                     actor.executeReceiveRequest(
-                            new Request(new Request.Path(conjunctive), list(), list(), list()),
+                            new Request(new Request.Path(conjunction), list(), list(), list()),
                             registry
                     )
             );
